50a4bbac9ce46b337c021e579bfa772d1f5fff81
Alexadr7u7 <alex.lazcano.alexander@gmail.com>
2024-02-11 11:47:17 -0600

    MAS CAMBIOS

--- login.php
+++ login.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<<<<<<< HEAD
 <html lang="en">
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>AdminLTE 3 | Log in</title>
+    <title>DAVIALEX | Login</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link
@@ -19,18 +18,10 @@
       href="plugins/icheck-bootstrap/icheck-bootstrap.min.css"
     />
     <!-- Theme style -->
-=======
-<html lang="es">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>login</title>
->>>>>>> e531fac7fe08b298a91759ec8878a16bee640737
     <link
       rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
     />
-<<<<<<< HEAD
     <link rel="stylesheet" href="dist/css/adminlte.min.css" />
     <style>
       .bg-mitad {
@@ -106,56 +97,5 @@
     <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- AdminLTE App -->
     <script src="dist/js/adminlte.min.js"></script>
-=======
-  </head>
-  <style>
-    .bg-mitad {
-      background: linear-gradient(to bottom, #212529 50%, #fff 50%);
-    }
-  </style>
-  <body
-    class="bg-mitad d-flex align-items-center justify-content-center vh-100"
-  >
-    <div class="card fw-bolder" style="width: 25rem">
-      <div class="card-body">
-        <div class="text-center">
-          <img src="img/user (1).png" alt="" width="160" class="mx-auto mb-4" />
-        </div>
-        <form class="">
-          <div class="mb-3">
-            <label for="usuario" class="fw-bolder form-label">Email:</label>
-            <input
-              type="text"
-              class="form-control"
-              id="usuario"
-              placeholder="Ingrese su usuario"
-            />
-          </div>
-          <div class="mb-3">
-            <label for="contrasena" class="fw-bolder form-label"
-              >Contraseña:</label
-            >
-            <input
-              type="password"
-              class="form-control"
-              id="contrasena"
-              placeholder="Ingrese su contraseña"
-            />
-          </div>
-          <div class="text-center">
-            <button type="submit" class="btn btn-primary fw-bolder">
-              Entrar
-            </button>
-            <div class="mb-3">
-              <a href="#">¿Olvidaste tu contraseña?</a>
-            </div>
-          </div>
-        </form>
-        <div></div>
-      </div>
-    </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
->>>>>>> e531fac7fe08b298a91759ec8878a16bee640737
   </body>
 </html>
